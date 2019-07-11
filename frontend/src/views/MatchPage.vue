2e16d1a94842851b8a3631b5f726845c3b29e2cd
Ellah Toren <ellahtoren@gmail.com>
2019-07-11 16:19:09 +0300

    added vuetify

--- frontend/src/views/MatchPage.vue
+++ frontend/src/views/MatchPage.vue
@@ -5,9 +5,9 @@
 </template>
 
 <script>
-    import ChatRoom from './ChatRoom.vue'
-    import ImageGallery from './ImageGallery.vue'
-    import TripList from './TripList.vue'
+    import ChatRoom from '@/components/ChatRoom.vue'
+    import ImageGallery from '@/components/ImageGallery.vue'
+    import TripList from '@/components/TripList.vue'
 
     export default {
         components: {
