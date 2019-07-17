74494daba97b2f94187bb6cb080a96a0c2a4707e
Maor Tovim <maortovim@gmail.com>
2019-07-17 12:15:29 +0300

    blllsss

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,8 +1,7 @@
 <template>
   <section class="homepage lato-light">
     <div class="container">
-      <h1 class="logo">
-        Mee<span class="Lato-bold">Travel</span>
+      <h1 class="logo">Mee<span class="Lato-bold">Travel</span>
       </h1>
       <h4 class>Meet people. Have an adventure.</h4>
       <div class="app-presenting">
@@ -11,22 +10,13 @@
       </div>
     </div>
     <section class="locations-container Lato-bold">
-      
-      <v-select
-        class="select"
-        @change="goToUsers"
-        :items="items"
-        v-model="location"
-        label="Top Locations"
-        single-line
-        hint="Most Popular"
-        item-text="city"
-        item-value="location"
-        return-object
-        autocomplete
-      ></v-select>
+      <h2>Top Locations</h2>
+      <button class="btn small primary-dark" @click="setLocation(32.109333, 34.855499)">Tel Aviv</button>
+      <button class="btn small primary-dark"  @click="setLocation(35.652832, 139.839478)">Tokyo</button>
+      <button class="btn small primary-dark" @click="setLocation(40.73061, -73.935242)">New York</button>
+
       <h4>OR</h4>
-      <div class="btn primary-dark shiny" @click.native="getUserLocation">
+      <div class="btn primary-dark shiny" @click="getUserLocation">
         <v-icon color="white" class="px-1">location_on</v-icon>Get My Location
         <span class="shiny"></span>
       </div>
@@ -43,31 +33,12 @@ export default {
     this.$emit("homepage", false);
   },
 
-  mounted() {
-    this.autocomplete = new google.maps.places.Autocomplete(
-      this.$refs.autocomplete,
-      { types: ["geocode"] }
-    );
-    this.autocomplete.addListener("place_changed", () => {
-      let place = this.autocomplete.getPlace();
-      let ac = place.address_components;
-      console.log(ac);
-      this.location.coords.lat = place.geometry.location.lat();
-      this.location.coords.lng = place.geometry.location.lng();
-      this.address = ac[0]["short_name"] + ", " + ac[2]["long_name"];
-    });
-  },
+
 
   data() {
     return {
-      location: { city: "", coords: { lat: null, lng: null } },
+      location: { lat: null, lng: null } ,
       address: null,
-      autocomplete: null,
-      items: [
-        { city: "Tel Aviv", coords: { lat: 32.109333, lng: 34.855499 } },
-        { city: "Tokyo", coords: { lat: 35.652832, lng: 139.839478 } },
-        { city: "New York", coords: { lat: 40.73061, lng: -73.935242 } }
-      ]
     };
   },
 
@@ -93,11 +64,18 @@ export default {
 
     getUserLocation() {
       GeocodeService.getcoordsition().then(loc => {
-        this.location.coords = loc.coords;
-        this.goToUsers();
+        console.log(loc);
+        this.location.lat = loc.coords.lat;
+        this.location.lng = loc.coords.lng;
+       // this.goToUsers();
       });
     },
-    setLocation() {},
+    setLocation(lat, lng) {
+      this.location.lat = lat;
+      this.location.lng = lng;
+      console.log(this.location);
+      this.goToUsers();
+    },
     goToUsers() {
       this.$router.push("/users");
     }
